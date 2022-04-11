ea4cd3db7acebcd0b249935e48152b1a57dad601
sebpalluel <sebpalluel@gmail.com>
2022-04-11 17:39:34 +0200

    added appolo to vue project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <HelloWorld />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld
+    },
 
-  data: () => ({
-    //
-  }),
-};
+    data: () => ({
+        //
+    })
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
 import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
+import App from './App.vue'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import App from './App.vue'
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:3020/graphql'
+})
+// Cache implementation
+const cache = new InMemoryCache()
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient
+})
 
 const app = createApp(App)
 const vuetify = createVuetify({
@@ -12,5 +36,7 @@ const vuetify = createVuetify({
 }) // Replaces new Vuetify(...)
 
 app.use(vuetify)
+
+app.use(apolloProvider)
 
 app.mount('#app')
