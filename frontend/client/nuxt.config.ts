089cf3b2e00c6c9230aebae3bb02b011ae80154d
sebpalluel <sebpalluel@gmail.com>
2022-05-13 18:31:52 +0200

    added vueuse to project

--- frontend/client/nuxt.config.ts
+++ frontend/client/nuxt.config.ts
@@ -2,7 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ['@nuxtjs/stylelint-module', '@nuxtjs/eslint-module'],
+    buildModules: [
+        '@nuxtjs/stylelint-module',
+        '@nuxtjs/eslint-module',
+        '@vueuse/nuxt'
+    ],
     css: ['vuetify/styles'],
     build: {
         transpile: ['vuetify', '@apollo/client', 'ts-invariant/process']
