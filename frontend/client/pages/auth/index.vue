2e09c39f5a2d06d70a80f33266cf22f047d30d96
sebpalluel <sebpalluel@gmail.com>
2022-05-25 17:19:26 +0200

    fix nuxt config

---
+++ frontend/client/pages/auth/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="d-flex flex-column">
+        <v-text-field v-model="formData.email" v-bind="{ label: 'email' }" />
+        <v-text-field v-model="formData.password" v-bind="{ label: 'password' }" />
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+            formData: {
+                email: '',
+                password: ''
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
