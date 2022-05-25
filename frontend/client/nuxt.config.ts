c4a4a6278917fd8aed551ad75bf69d2ab70156be
sebpalluel <sebpalluel@gmail.com>
2022-05-25 15:56:19 +0200

    fix nuxt client + add axios support

--- frontend/client/nuxt.config.ts
+++ frontend/client/nuxt.config.ts
@@ -8,7 +8,27 @@ export default defineNuxtConfig({
         '@vueuse/nuxt',
         '@pinia/nuxt'
     ],
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        ['@nuxtjs/axios', { proxyHeaders: false }]
+    ],
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/'
+    },
+    publicRuntimeConfig: {
+        axios: {
+            baseURL: 'http://localhost:8000/api/'
+        }
+    },
+    // https://nuxtjs.org/docs/features/loading/
+    loading: {
+        color: 'blue',
+        height: '5px'
+    },
     css: ['vuetify/styles'],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
         transpile: ['vuetify', '@apollo/client', 'ts-invariant/process']
     },
