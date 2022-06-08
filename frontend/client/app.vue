e1b4699eec7cd2a22c7a44c34028643f77bff9e9
sebpalluel <sebpalluel@gmail.com>
2022-06-08 18:23:54 +0200

    trying to fix vscode config monrepo

--- frontend/client/app.vue
+++ frontend/client/app.vue
@@ -1,16 +1,11 @@
+<script setup lang="ts">
+import { useApolloClient } from '@/composables/useApolloClient'
+// TODO add token to headers on init when loggedin and restore when logged out
+useApolloClient()
+</script>
+
 <template>
     <div>
         <NuxtPage />
     </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useApolloClient } from '@/composables/useApolloClient'
-export default defineComponent({
-    async setup() {
-        // TODO add token to headers on init when loggedin and restore when logged out
-        useApolloClient()
-    }
-})
-</script>
--- frontend/client/store/user.spec.ts
+++ frontend/client/store/user.spec.ts
@@ -2,19 +2,34 @@ import { setup } from '@nuxt/test-utils-edge'
 import { fileURLToPath } from 'node:url'
 import { useUserStore } from './user'
 import { createPinia } from 'pinia'
+import { mount } from '@vue/test-utils'
+import Login from '@/pages/auth/index.vue'
 
 describe('User store', async () => {
-    await setup({
-        rootDir: fileURLToPath(new URL('..', import.meta.url)),
-        server: true
+    // await setup({
+    //     rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    //     server: true,
+    //     browser: true
+    // })
+    afterEach(() => {
+        vi.restoreAllMocks()
     })
     test('login', async () => {
         const formData = {
             email: '',
             password: ''
         }
-        const store = useUserStore(createPinia())
-        console.log({ store })
+
+        const wrapper = mount(Login, {})
+        const button = wrapper.find('button')
+        await button.trigger('click')
+        console.log({ wrapper })
+        // const store = useUserStore(createPinia())
+        // console.log({ store })
+        // expect(store.login).toBeDefined()
+        // const mockApi = vi.fn().mockImplementation('this.$nuxt.$axios.post')
+        // await store.login()
+        // expect(mockApi).toHaveBeenCalled()
 
         // const res = await $nuxt.$axios.post('/token/', formData)
     })
