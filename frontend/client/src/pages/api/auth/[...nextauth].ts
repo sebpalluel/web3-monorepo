59d4cb17ae047c3054e5fe2a09ac79ab6364d572
sebpalluel <sebpalluel@gmail.com>
2022-07-08 18:42:23 +0200

    trying to implement mail login + mailcatcher

--- frontend/client/src/pages/api/auth/[...nextauth].ts
+++ frontend/client/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email";
 import CredentialsProvider from "next-auth/providers/credentials"
 
 import { HasuraAdapter, hasuraRequest } from "../../../lib/hasuraAdapter"
@@ -18,38 +19,68 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      id: 'credentials',
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        const res = hasuraRequest()
-        const user = await res.json()
-
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user
+    // https://next-auth.js.org/configuration/providers/email
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
         }
-        // Return null if user data could not be retrieved
-        return null
-      }
-    })
+      },
+      from: process.env.EMAIL_FROM
+    }),
+    // CredentialsProvider({
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     email: { label: "Email", type: "email" },
+    //     password: { label: "Password", type: "password" }
+    //   },
+    //   async authorize(credentials, req) {
+    //     console.log({credentials, req});
+
+    //     // You need to provide your own logic here that takes the credentials
+    //     // submitted and returns either a object representing a user or value
+    //     // that is false/null if the credentials are invalid.
+    //     // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+    //     // You can also use the `req` object to obtain additional parameters
+    //     // (i.e., the request IP address)
+    //     const res = hasuraRequest({
+    // 		query: `
+    //       query getUser($id: Int!){
+    //         users(where: {id: {_eq: $id}}) {
+    //           id
+    //           email
+    //         }
+    //       }
+    //     `,
+    // 		variables: {
+    // 			id
+    // 		},
+    // 		admin: true,
+    // 	})
+    //     const user = await res.json()
+
+    //     // If no error and we have user data, return it
+    //     if (res.ok && user) {
+    //       return user
+    //     }
+    //     // Return null if user data could not be retrieved
+    //     return null
+    //   }
+    // })
   ],
   adapter: HasuraAdapter(),
+  pages: {
+    // signIn: '/auth/signin',
+    // signOut: '/auth/signout',
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   theme: {
     colorScheme: "light",
   },
