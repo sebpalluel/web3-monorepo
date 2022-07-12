f7f414b9d8963723dc8cfad03c7903b62e31963d
sebpalluel <sebpalluel@gmail.com>
2022-07-12 19:27:50 +0200

    set back hasuraRequest in hasura route

--- frontend/client/src/pages/api/examples/hasura.ts
+++ frontend/client/src/pages/api/examples/hasura.ts
@@ -1,13 +1,11 @@
-import { hasuraRequest } from "lib/hasuraAdapter"
+import { hasuraRequest } from 'lib/hasuraAdapter'
 
-import { getToken } from "next-auth/jwt"
+import { getToken } from 'next-auth/jwt'
 
-import { getSession } from "next-auth/react"
-
-import { useGetUserQuery } from "generated/user-gql"
+import { getSession } from 'next-auth/react'
 
 // This is an example of how to read a JSON Web Token from an API route
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 const secret = process.env.NEXTAUTH_SECRET
 
@@ -15,31 +13,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req })
   const token = await getToken({ req, secret, raw: true })
   if (session && token) {
-    const { status, data, error, isFetching } = await useGetUserQuery({
-      endpoint: process.browser ? process.env.HASURA_URL : process.env.HASURA_SSR_URL,
-      fetchParams: {
-        headers: { 'Authorization': `Bearer ${token}` }
-      },
+    const data = await hasuraRequest({
+      token,
+      query: `
+            query getUser($id: String!){
+                users(where: {id: {_eq: $id}}) {
+                    id
+                    email
+              }
+            }
+       `,
       variables: {
         id: session?.user?.id
       }
     })
-    // const data = await hasuraRequest({
-    //   token: token,
-    //   query: `
-    //         query getUser($id: String!){
-    //             users(where: {id: {_eq: $id}}) {
-    //                 id
-    //                 email
-    //           }
-    //         }
-    //    `,
-    //   variables: {
-    //     id: session?.user?.id
-    //   },
-    // });
     res.send({ me: data?.users[0] })
   } else {
-    res.send({ error: 'You must be sign in to view the protected content on this page.' })
+    res.send({
+      error: 'You must be sign in to view the protected content on this page.'
+    })
   }
 }
