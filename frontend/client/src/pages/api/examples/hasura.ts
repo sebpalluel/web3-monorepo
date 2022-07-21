967745797fd726082a63bb78f8e4fed16e57154d
sebpalluel <sebpalluel@gmail.com>
2022-07-21 15:39:22 +0200

    check husky pre-commit

--- frontend/client/src/pages/api/examples/hasura.ts
+++ frontend/client/src/pages/api/examples/hasura.ts
@@ -10,14 +10,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const secret = process.env.NEXTAUTH_SECRET
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req })
-  const token = await getToken({ req, secret, raw: true })
-  if (session && token) {
-    console.log({token});
-    
-    const data = await hasuraRequest({
-      token,
-      query: `
+    const session = await getSession({ req })
+    const token = await getToken({ req, secret, raw: true })
+    if (session && token) {
+        console.log({ token })
+
+        const data = await hasuraRequest({
+            token,
+            query: `
             query getUser($id: String!){
                 users(where: {id: {_eq: $id}}) {
                     id
@@ -25,14 +25,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
               }
             }
        `,
-      variables: {
-        id: session?.user?.id
-      }
-    })
-    res.send({ me: data?.users[0] })
-  } else {
-    res.send({
-      error: 'You must be sign in to view the protected content on this page.'
-    })
-  }
+            variables: {
+                id: session?.user?.id
+            }
+        })
+        res.send({ me: data?.users[0] })
+    } else {
+        res.send({
+            error: 'You must be sign in to view the protected content on this page.'
+        })
+    }
 }
