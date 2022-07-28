599c35815b524b4ffa56f9fee66f208460f2ac8e
sebpalluel <sebpalluel@gmail.com>
2022-07-28 19:14:20 +0200

    moved hasura-console to root folder

--- hasura-console/start.sh
+++ hasura-console/start.sh
@@ -8,9 +8,9 @@ cd $HASURA_FOLDER || {
 }
 
 # Workaround for https://github.com/hasura/graphql-engine/issues/2824#issuecomment-801293056
-socat TCP-LISTEN:$HASURA_PORT,fork TCP:backend-hasura-engine:$HASURA_PORT &
-socat TCP-LISTEN:$HASURA_CONSOLE_PORT,fork,reuseaddr,bind=backend-hasura-console TCP:127.0.0.1:$HASURA_CONSOLE_PORT &
-socat TCP-LISTEN:9693,fork,reuseaddr,bind=backend-hasura-console TCP:127.0.0.1:9693 &
+socat TCP-LISTEN:$HASURA_PORT,fork TCP:hasura-engine:$HASURA_PORT &
+socat TCP-LISTEN:$HASURA_CONSOLE_PORT,fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:$HASURA_CONSOLE_PORT &
+socat TCP-LISTEN:9693,fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:9693 &
 {
     # Apply migrations
     $HASURA_PATH migrate apply --database-name=default || exit 1
