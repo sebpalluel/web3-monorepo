46b44f34956271ae5b380bf7575554c4276bc1fb
sebpalluel <sebpalluel@gmail.com>
2022-08-15 13:58:30 +0200

    removing admin from allowed roles in hasura

--- libs/hasura/utils/src/index.ts
+++ libs/hasura/utils/src/index.ts
@@ -1,11 +1,9 @@
 export enum Roles {
   user = 'user',
-  admin = 'admin',
   anonymous = 'anonymous',
 }
 
 export type UserRole = Roles.user;
-export type AdminRole = Roles.admin;
 export type AnonymousRole = Roles.anonymous;
 
-export type Role = UserRole | AdminRole | AnonymousRole;
+export type Role = UserRole | AnonymousRole;
