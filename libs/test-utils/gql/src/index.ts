c243ea73803b30cfea431436e858c862258a886f
sebpalluel <sebpalluel@gmail.com>
2022-08-15 17:06:11 +0200

    moved users to gql utils to avoid build issue

--- libs/test-utils/gql/src/index.ts
+++ libs/test-utils/gql/src/index.ts
@@ -1,12 +1,41 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { GraphQLClient } from 'graphql-request';
-import { users } from '../../../../tools/test/data/users';
 const jwt = require('jsonwebtoken');
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { getSdk as userSdk, Users } from '@governance/gql-user';
 
 // setup env variables
 require('dotenv').config({ path: './tools/test/.env.test' });
+
+export const users = {
+  seb_google: {
+    email: 'sebpalluel@gmail.com',
+    firstName: null,
+    id: '20c0bc91e1254445d459fc6ac97206f6bb9223e71c764c49a778f8b84d3fc57f',
+    image:
+      'https://lh3.googleusercontent.com/a-/AFdZucpuViPxV1AhiHmm1CalmByGnHAJemRH6MoCaePMEf0=s96-c',
+    lastName: null,
+    name: 'Sébastien Palluel',
+    emailVerified: null,
+  },
+  alpha_admin: {
+    email: 'alpha_admin@governance.io',
+    firstName: null,
+    id: '4c2aa03a7dcb06ab7ac2ba0783d2e466a525e1e5794a42b2a0fa9f61fa7a2965',
+    image: null,
+    lastName: null,
+    name: 'Alpha Admin',
+    emailVerified: null,
+  },
+  beta_admin: {
+    email: 'beta_admin@governance.io',
+    firstName: null,
+    id: '1d6dead4e698ddfd4a92cd19afd075611feaedfd149edd7462b80f718e3b2183',
+    image: null,
+    lastName: null,
+    name: 'Beta Admin',
+    emailVerified: null,
+  },
+};
 
 type UserOptions = {
   allowedRoles?: string[];
