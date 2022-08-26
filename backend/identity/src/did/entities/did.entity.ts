379faea8d015d4e6cf2d458e562e4e86c83bc048
sebpalluel <sebpalluel@gmail.com>
2022-08-26 12:15:53 +0200

    delete backend identity

--- backend/identity/src/did/entities/did.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
-@ObjectType()
-export class Did {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}
