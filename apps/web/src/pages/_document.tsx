5ac2a3ccd7b2fcf53789444d8e46d4a4cceef289
sebpalluel <sebpalluel@gmail.com>
2022-10-18 12:34:59 +0200

    update web app ref

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import customTheme from '../lib/styles/customTheme';
 
-const APP_NAME = 'nextarter-chakra';
+const APP_NAME = 'web3-monorepo';
 
 class MyDocument extends Document {
   static getInitialProps(ctx: DocumentContext) {
