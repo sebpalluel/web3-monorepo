88bd7bbb4868b879eccd70cff97e13b9420660c2
sebpalluel <sebpalluel@gmail.com>
2022-10-18 12:34:59 +0200

    update web app ref

--- apps/web/next-seo.config.js
+++ apps/web/next-seo.config.js
@@ -1,25 +1,23 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: 'nextarter-chakra',
-  titleTemplate: '%s | nextarter-chakra',
-  defaultTitle: 'nextarter-chakra',
-  description: 'Next.js + chakra-ui + TypeScript template',
-  canonical: 'https://nextarter-chakra.sznm.dev',
+  title: 'web3-monorepo',
+  titleTemplate: '%s | web3-monorepo',
+  defaultTitle: 'web3-monorepo',
+  description:
+    '⚡ Get started with your Web3 project,🔋 battery included: 🪪 DID + SSI from Walt.id, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI for the frontend, 💾 Hasura GraphQL server for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
+  canonical: 'https://web3-monorepo.dev',
   openGraph: {
-    url: 'https://nextarter-chakra.sznm.dev',
-    title: 'nextarter-chakra',
-    description: 'Next.js + chakra-ui + TypeScript template',
+    url: 'https://web3-monorepo.dev',
+    title: 'web3-monorepo',
+    description:
+      '⚡ Get started with your Web3 project,🔋 battery included: 🪪 DID + SSI from Walt.id, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI for the frontend, 💾 Hasura GraphQL server for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
     images: [
       {
-        url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
-        alt: 'nextarter-chakra.sznm.dev og-image',
+        url: 'https://og-image.dev/**web3-monorepo**.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
+        alt: 'web3-monorepo.dev og-image',
       },
     ],
-    site_name: 'nextarter-chakra',
-  },
-  twitter: {
-    handle: '@sozonome',
-    cardType: 'summary_large_image',
+    site_name: 'web3-monorepo',
   },
 };
 
--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-  siteUrl: 'https://nextarter-chakra.sznm.dev',
+  siteUrl: 'https://web3-monorepo.dev',
   generateRobotsTxt: true,
 };
 
