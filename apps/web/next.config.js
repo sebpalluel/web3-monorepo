f1fb860b3c82997509504e164226be11b99bc3f3
sebpalluel <sebpalluel@gmail.com>
2022-10-19 17:19:54 +0200

    upgraded packages + resolve new pwa config in next js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa');
 const withNx = require('@nrwl/next/plugins/with-nx');
 
 /**
@@ -7,22 +6,23 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  * */
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable:
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'preview' ||
+    process.env.NODE_ENV === 'production',
+  // delete two lines above to enable PWA in production deployment
+  // add your own icons to public/manifest.json
+  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+  register: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development',
-  },
-  pwa: {
-    disable:
-      process.env.NODE_ENV === 'development' ||
-      process.env.NODE_ENV === 'preview' ||
-      process.env.NODE_ENV === 'production',
-    // delete two lines above to enable PWA in production deployment
-    // add your own icons to public/manifest.json
-    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-    dest: 'public',
-    register: true,
   },
   nx: {
     // Set this to true if you would like to to use SVGR
