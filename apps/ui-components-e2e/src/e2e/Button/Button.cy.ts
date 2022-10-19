e45830ab7d6be63980ef7432b52a5470a23392fe
sebpalluel <sebpalluel@gmail.com>
2022-10-19 18:12:23 +0200

    fix e2e test ui component

--- apps/ui-components-e2e/src/e2e/Button/Button.cy.ts
+++ apps/ui-components-e2e/src/e2e/Button/Button.cy.ts
@@ -2,6 +2,6 @@ describe('ui-components: Button component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=button--primary'));
 
   it('should render the component', () => {
-    cy.get('h1').should('contain', 'Welcome to Button!');
+    cy.get('h1').should('contain', 'Button');
   });
 });
