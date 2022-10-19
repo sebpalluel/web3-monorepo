da0db6bc02ffd0191e72cc516ac3a7ca43ae63f9
sebpalluel <sebpalluel@gmail.com>
2022-10-19 15:09:33 +0200

    apply fix to avoid displaying css theming controls from chakra component on stories + example basic with own button component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,30 @@
 import type { StorybookConfig } from '@storybook/core-common';
+import { propNames } from '@chakra-ui/react';
+// https://github.com/chakra-ui/chakra-ui/issues/2009#issuecomment-852793946
+const excludedPropNames = propNames.concat(['as', 'apply', 'sx', '__css']);
 
 export const rootMain: StorybookConfig = {
   stories: [],
   addons: ['@storybook/addon-essentials', '@chakra-ui/storybook-addon'],
   features: {
     emotionAlias: false,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => {
+        const isStyledSystemProp = excludedPropNames.includes(prop.name);
+        const isHTMLElementProp = prop.parent?.fileName.includes('node_modules') ?? false;
+        return !(isStyledSystemProp || isHTMLElementProp);
+      },
+    },
   },
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
