122593cebe5d256a0b958838043f821cea4dad07
sebpalluel <sebpalluel@gmail.com>
2022-10-21 12:52:30 +0200

    First feature Btn clicked with auto loading + first interaction test

--- libs/test-utils/common/src/index.ts
+++ libs/test-utils/common/src/index.ts
@@ -5,3 +5,12 @@ export function isJestRunning() {
 export function isCypressRunning() {
   return window && (window as any).Cypress !== undefined;
 }
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function delayData(ms = 2000, data: any) {
+  await delay(ms);
+  return data;
+}
