ac15eb6f91d773e35c023323f66cc0735f61c2fa
sebpalluel <sebpalluel@gmail.com>
2022-12-08 18:22:42 +0100

    set back function getNextAuthURL

--- libs/client/next-auth/common/src/lib/index.ts
+++ libs/client/next-auth/common/src/lib/index.ts
@@ -1,3 +1,9 @@
+import { isProd } from '@utils';
+
 export function getNextAuthURL(): string {
-  return process.env.VERCEL_URL || 'http://localhost:3000';
+  if (isProd()) {
+    return process.env.NEXTAUTH_URL as string;
+  } else {
+    return process.env.VERCEL_URL || 'http://localhost:3000';
+  }
 }
