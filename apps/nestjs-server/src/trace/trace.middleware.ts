a25547391a7dd190bb9804ad012671141a3bd1a2
sebpalluel <sebpalluel@gmail.com>
2022-12-09 11:20:12 +0100

    added sentry with tracing in nestjs server + restart redis container unless stopped

---
+++ apps/nestjs-server/src/trace/trace.middleware.ts
@@ -0,0 +1,38 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { InjectSentry, SentryService } from '@ntegral/nestjs-sentry';
+
+@Injectable()
+export class TraceMiddleware implements NestMiddleware {
+  constructor(@InjectSentry() private readonly sentry: SentryService) {}
+  use(req: Request, res: Response, next: NextFunction): void {
+    const transaction = this.sentry.instance().startTransaction({
+      op: 'request',
+      name: req.url,
+    });
+
+    this.sentry
+      .instance()
+      .getCurrentHub()
+      .configureScope((scope) => {
+        scope.addEventProcessor((event) => {
+          event.request = {
+            method: req.method,
+            url: req.baseUrl,
+          };
+          return event;
+        });
+      });
+
+    this.sentry.instance().configureScope((scope) => {
+      scope.setSpan(transaction);
+    });
+
+    req.on('close', () => {
+      transaction.setHttpStatus(res.statusCode);
+      transaction.finish();
+    });
+
+    next();
+  }
+}
