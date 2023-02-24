93b02ca8794f3c7f07ed2d10601583fbde7cc292
sebpalluel <sebpalluel@gmail.com>
2023-02-24 18:25:21 +0100

    typo on getNextAuthURL

--- libs/client/next-auth/common/src/lib/index.ts
+++ libs/client/next-auth/common/src/lib/index.ts
@@ -1,7 +1,8 @@
 import { isProd, isServerSide } from '@utils';
 
 export function getNextAuthURL(): string {
-  const vercelURL = process.env.VERCEL_URL || `https://${NEXT_PUBLIC_VERCEL_URL}`;
+  const vercelURL =
+    process.env.VERCEL_URL || `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   if (isProd()) {
     if (process.env.VERCEL_GITHUB_DEPLOYMENT) return vercelURL;
     return isServerSide() ? (process.env.NEXTAUTH_URL as string) : vercelURL;
