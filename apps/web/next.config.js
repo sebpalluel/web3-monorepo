502c397ae6df095e3ad300c13c26486fa59fed4f
sebpalluel <sebpalluel@gmail.com>
2023-02-25 19:00:26 +0100

    try with this config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,83 @@
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// const withNx = require('@nrwl/next/plugins/with-nx');
+// const path = require('path');
+// const { withSentryConfig } = require('@sentry/nextjs');
+
+// const SENTRY_DSN = process.env.SENTRY_AUTH_TOKEN
+//   ? null
+//   : process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV === 'production',
+//   },
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         fs: false,
+//         net: false,
+//         tls: false,
+//         // crypto: require.resolve('crypto-browserify'),
+//       };
+//     }
+//     // resolve ts issues with nextjs build (bug occured after upgrade to 13.2)
+//     config.module.rules.push({
+//       test: /\.tsx?$/,
+//       exclude: /node_modules/,
+//       use: {
+//         loader: 'babel-loader',
+//         options: {
+//           presets: ['next/babel'],
+//         },
+//       },
+//     });
+
+//     return config;
+//   },
+//   images: {},
+//   // Use the CDN in production and localhost for development.
+//   // assetPrefix: isProd() ? 'https://cdn.mydomain.com' : undefined,
+//   assetPrefix: undefined,
+//   // optimize build with vercel nft (node file tracing) https://nextjs.org/docs/advanced-features/output-file-tracing
+//   // outputFileTracingRoot needed for monorepo
+//   // output: 'standalone',
+//   experimental: {
+//     // outputFileTracingRoot: path.join(__dirname, '../../'),
+//     // to fix chakra ui error with cancelSync esm import
+//     esmExternals: false,
+//     // for now causing issue in vercel build so set to false
+//     typedRoutes: false,
+//     appDir: false,
+//     //
+//   },
+//   //
+//   sentry: {
+//     // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+//     // for client-side builds. (This will be the default starting in
+//     // `@sentry/nextjs` version 8.0.0.) See
+//     // https://webpack.js.org/configuration/devtool/ and
+//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+//     // for more information.
+//     hideSourceMaps: true,
+//   },
+// };
+
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+//   dryRun: !SENTRY_DSN,
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
+
+// module.exports = withSentryConfig(withNx(nextConfig), sentryWebpackPluginOptions);
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withNx = require('@nrwl/next/plugins/with-nx');
 const path = require('path');
@@ -22,18 +102,17 @@ const nextConfig = {
         // crypto: require.resolve('crypto-browserify'),
       };
     }
-    // resolve ts issues with nextjs build (bug occured after upgrade to 13.2)
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-
+    // //  resolve ts issues with nextjs build (bug occured after upgrade to 13.2)
+    // config.module.rules.push({
+    //   test: /\.tsx?$/,
+    //   exclude: /node_modules/,
+    //   use: {
+    //     loader: 'babel-loader',
+    //     options: {
+    //       presets: ['next/babel'],
+    //     },
+    //   },
+    // });
     return config;
   },
   images: {},
@@ -47,10 +126,7 @@ const nextConfig = {
     outputFileTracingRoot: path.join(__dirname, '../../'),
     // to fix chakra ui error with cancelSync esm import
     esmExternals: false,
-    // for now causing issue in vercel build so set to false
-    typedRoutes: false,
-    appDir: false,
-    //
+    // typedRoutes: true,
   },
   //
   sentry: {
