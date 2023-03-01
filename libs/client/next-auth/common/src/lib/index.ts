59cc4402de0d5bc7f956b607963d34d18682cf23
sebpalluel <sebpalluel@gmail.com>
2023-03-01 12:04:04 +0100

    fix issue on getNextAppURL with vercelURL

--- libs/client/next-auth/common/src/lib/index.ts
+++ libs/client/next-auth/common/src/lib/index.ts
@@ -3,9 +3,9 @@ import { isProd, isServerSide, isPreviewOrPRDeployment } from '@utils';
 export function getNextAppURL(): string {
   let vercelURL = '';
   if (process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL)
-    vercelURL =
-      `https://${process.env.VERCEL_URL}` ||
-      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    vercelURL = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   if (isProd()) {
     if (isPreviewOrPRDeployment()) return vercelURL;
     return isServerSide() ? (process.env.NEXTAUTH_URL as string) : vercelURL;
