7fd8b1436309f539e6dc4866adaf1b7ba90acf62
sebpalluel <sebpalluel@gmail.com>
2023-03-02 10:20:46 +0100

    migrate to latest nx version 15.8.1

--- libs/client/hasura/adapter/jest.config.ts
+++ libs/client/hasura/adapter/jest.config.ts
@@ -1,15 +1,16 @@
 export default {
   displayName: 'client-hasura-adapter',
   preset: '../../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   globalSetup: `${process.cwd()}/tools/test/globalSetupHasura.ts`,
   globalTeardown: `${process.cwd()}/tools/test/globalTeardownHasura.ts`,
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/client/hasura/adapter',
--- libs/client/hasura/fetcher/jest.config.ts
+++ libs/client/hasura/fetcher/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'client-hasura-fetcher',
   preset: '../../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/client/hasura/fetcher',
