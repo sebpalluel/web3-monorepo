cbd78d4d09c77f74c74754d8d4e95d1694861868
sebpalluel <sebpalluel@gmail.com>
2023-03-02 18:29:38 +0100

    moved vercelURL outside of isProd to see if it resolve the issue

--- libs/client/next-auth/common/src/lib/index.ts
+++ libs/client/next-auth/common/src/lib/index.ts
@@ -1,17 +1,16 @@
 import { isProd, isServerSide } from '@utils';
 
 export function getNextAppURL(): string {
-  let vercelURL = '';
-  if (process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL)
-    vercelURL = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   if (isProd()) {
-    if (vercelURL) return vercelURL;
     return isServerSide()
       ? (process.env.NEXTAUTH_URL as string)
       : (process.env.NEXT_PUBLIC_SITE_URL as string);
   } else {
+    let vercelURL = '';
+    if (process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL)
+      vercelURL = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     return vercelURL || 'http://localhost:3000';
   }
 }
