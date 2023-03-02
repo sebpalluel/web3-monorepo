8093ac2c78125fe6d61b6c20fab64b73833ce859
sebpalluel <sebpalluel@gmail.com>
2023-03-02 12:28:54 +0100

    update seo config

--- apps/web/next-seo.config.js
+++ apps/web/next-seo.config.js
@@ -4,14 +4,15 @@ const defaultSEOConfig = {
   titleTemplate: '%s | web3-monorepo',
   defaultTitle: 'web3-monorepo',
   description:
-    '⚡️ Get started with your Web3 project,🔋 included: 🪪 DID + SSI from Walt.id, 🌈 RainbowKit, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI + Storybook for the frontend, 💾 Hasura GraphQL server and Nest.js for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
+    '⚡️ Get started with your Web3 project,🔋 included: 🌈 Web3Auth & Biconomy for non-custodial Social Login and Account Abstraction, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI + Storybook for the frontend, 💾 Hasura GraphQL server and Nest.js for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
   canonical: 'https://www.web3-monorepo.app/',
   openGraph: {
     url: 'https://www.web3-monorepo.app/',
     title: 'web3-monorepo',
     description:
-      '⚡️ Get started with your Web3 project,🔋 included: 🪪 DID + SSI from Walt.id, 🌈 RainbowKit, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI + Storybook for the frontend, 💾 Hasura GraphQL server and Nest.js for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
+      '⚡️ Get started with your Web3 project,🔋 included: 🌈 Web3Auth & Biconomy for non-custodial Social Login and Account Abstraction, 🗂 monorepo with NX, 💻 Next.js + Next Auth + ChakraUI + Storybook for the frontend, 💾 Hasura GraphQL server and Nest.js for the backend, 🔮 The Graph protocol to query live data from smart contracts.',
     images: [
+      // TODO, update with hosted image
       {
         url: 'https://og-image.dev/**web3-monorepo**.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
         alt: 'web3-monorepo.dev og-image',
